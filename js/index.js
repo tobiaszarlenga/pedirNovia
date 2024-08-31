c988f9ea5ac543e3ac37cbd22c88848d0897799b
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2024-08-31 14:24:22 -0300

    agrego imagen

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.getElementById('continuarBtn').addEventListener('click', function() {
     document.body.classList.add('fade-out');
     setTimeout(function() {
-      window.location.href = './pages/sorpresa.html';
+      window.location.href = './pages/paginaSorpresa.html';
     }, 500); // Espera a que la transición termine
 });
