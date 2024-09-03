057a83c2d0d4de67c30823969350571b104cbc9f
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2024-09-03 13:00:46 -0300

    cambios index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
+  console.log("paguina loaded");
   document.body.classList.add("fade-in");
 
   const button = document.getElementById("continuarBtn");
@@ -13,26 +14,26 @@ window.addEventListener("load", function () {
 
         switch (currentStep) {
           case "step1":
-            nextPage = "pages/paginaSorpresa.html";
+            nextPage = "../pages/paginaSorpresa.html";
             localStorage.setItem("progressStep", "step2");
             break;
           case "step2":
-            nextPage = "pages/cartaDeAmor.html";
+            nextPage = "../pages/cartaDeAmor.html";
             localStorage.setItem("progressStep", "step3");
             break;
           case "step3":
-            nextPage = "pages/serMiNovia.html";
+            nextPage = "../pages/serMiNovia.html";
             localStorage.setItem("progressStep", "step4");
             break;
           case "step4":
-            nextPage = "index.html";
+            nextPage = "../index.html";
             localStorage.removeItem("progressStep"); // Reset progress for future visits
             break;
           default:
-            nextPage = "index.html";
+            nextPage = "../index.html";
             break;
         }
-
+        
         window.location.href = nextPage;
       }, 1500);
     });
@@ -43,16 +44,18 @@ window.addEventListener("load", function () {
 document.addEventListener("DOMContentLoaded", () => {
   const siBtn = document.getElementById("SiBtn");
 
-  if (siBtn) {
-    siBtn.addEventListener("click", (event) => {
-      event.preventDefault();
-      document.body.classList.add("fade-out");
+if (siBtn) {
+  siBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.body.classList.add("fade-out");
 
-      setTimeout(() => {
-        window.location.href = "pages/fin.html";
-      }, 1500);
-    });
-  }
+    setTimeout(() => {
+      // Ajuste la ruta para asegurarse de que apunta correctamente a la página fin.html
+      window.location.href = "../pages/fin.html";
+    }, 1500);
+  });
+}
+
 
   const noBtn = document.getElementById("NoBtn");
   if (noBtn) {
